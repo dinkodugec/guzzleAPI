67b62e07d6bdbcf9e53396c387044e1a9be99d07
Dinko <dinko.dugec@gmail.com>
2022-10-16 21:30:21 +0200

    basic request of all users

--- codes/index.php
+++ codes/index.php
@@ -3,10 +3,14 @@
 require '../vendor/autoload.php';
 use GuzzleHttp\Client;
 
-$client = new Client();
+$client = new Client(['base_uri' => 'http://jsonplaceholder.typicode.com/']);
 
-$response = $client->request('GET', 'https://jsonplaceholder.typicode.com/posts/1');
+$response = $client->request('GET', 'posts/1');
 
-var_dump($response);
+/* var_dump($response); */
 
 echo $response->getBody();
+
+
+$response1 = $client->get('posts/1');
+echo $response1->getBody() . "\r\n";
